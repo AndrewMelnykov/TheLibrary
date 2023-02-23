3cdb2a230ad81b2eea305b6e55782d3f048a9fd3
Andrey Melnykov <molot361@gmail.com>
2023-02-23 18:37:10 +0100

    updated templates

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/home')
 def home():
     return render_template("home.html")
+
+@app.route('/mybooks')
+def my_books():
+    return render_template("mybooks.html")
+
+@app.route('/authors')
+def authors():
+    return render_template("authors.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
