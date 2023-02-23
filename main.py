b08c37fa076d89884f74b42f50157e01cdbeaa71
Andrey Melnykov <molot361@gmail.com>
2023-02-23 16:28:30 +0100

    updated templates

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
