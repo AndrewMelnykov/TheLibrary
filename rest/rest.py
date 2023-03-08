f0f0e1ca87da9350a4c2465ca2ae570e068e70af
Andrey Melnykov <molot361@gmail.com>
2023-03-08 17:08:14 +0100

    PUT methods in api

--- rest/rest.py
+++ rest/rest.py
@@ -1,11 +1,12 @@
-from main import app
+from main import app, db
 from models.db_models import Users, Review, Book, Library
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from flask_login import current_user, login_required
 
 api_routes = Blueprint("api_routes", __name__)
 
-@api_routes.route('/api/get/allbooks')
+#endpoint for getting all books
+@api_routes.route('/api/get/allbooks', methods=['GET'])
 def api_books():
 
     books = Book.query.order_by(Book.id).all()
@@ -19,9 +20,10 @@ def api_books():
                       'genre': book.genre,
                       'description': book.description}
 
-    return result_array
+    return jsonify(result_array)
 
-@app.route('/api/get/mybooks')
+#endpoint for getting user's books from db
+@api_routes.route('/api/get/mybooks', methods=['GET'])
 @login_required
 def api_my_books():
 
@@ -38,4 +40,33 @@ def api_my_books():
                    'description': Book.query.filter_by(id=library.book_id).first().description}
         i+=1
 
-    return result_array
+    return jsonify(result_array)
+
+#endpoint for deleting the book from db
+@api_routes.route('/api/delete/book/<id>', methods=["GET","DELETE"])
+@login_required
+def api_delete_book(id):
+
+    book = Book.query.get_or_404(id)
+
+    if book:
+        db.session.delete(book)
+        db.session.commit()
+
+    return "Book deleted!"
+
+#endpoint for updating the book info
+@api_routes.route('/api/edit/book/<id>', methods=["GET","PUT"])
+def api_edit_book(id):
+
+    book = Book.query.get_or_404(id)
+    if book:
+        author_name = request.json['author_name']
+        author_surname = request.json['author_surname']
+
+        book.author_name = author_name
+        book.author_surname = author_surname
+
+        db.session.commit()
+
+    return "Success"
